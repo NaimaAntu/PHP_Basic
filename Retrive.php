a78b0362872c81d8e4af7ccaea9b02f6e1cbff96
NaimaAntu <naimaahmed181460@gmail.com>
2025-08-02 23:50:48 +0600

    Add files via upload

---
+++ Retrive.php
@@ -0,0 +1,14 @@
+<?php 
+include 'connect.php'; 
+$sql = "SELECT * FROM users"; 
+$result = $conn->query($sql); 
+if ($result->num_rows > 0) { 
+while($row = $result->fetch_assoc()) { 
+echo "ID: " . $row["id"]. " - Name: " . $row["name"]. " - 
+Email: " . $row["email"]. "<br>"; 
+} 
+} else { 
+echo "0 results"; 
+} 
+$conn->close(); 
+?>
---
+++ insert.php
@@ -0,0 +1,9 @@
+<?php
+$sql = "INSERT INTO users (name, email) VALUES ('John Doe', 'john@example.com')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
